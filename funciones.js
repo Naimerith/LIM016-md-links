eb2e8f7c6563171f0ba4796a66eda5c02651c872
Naimerith <naimerith89@gmail.com>
2022-01-07 21:44:14 -0500

    Se creo la funcion recursiva que lista carpetas y archivos y filtra por md

--- funciones.js
+++ funciones.js
@@ -7,19 +7,36 @@ const pathExists = function (ruta) {
 }
 ////////// CONVIERTO LA RUTA EN ABSOLUTA /////////// 
 const convertPathInAbsolute = (ruta) => {
-    if (path.isAbsolute(ruta)) {
-      return ruta
-    }
-    return path.resolve(ruta)
+  if (path.isAbsolute(ruta)) {
+    return ruta
   }
+  return path.resolve(ruta)
+}
 //////////PREGUNTO SI ES UN DIRECTORIO///////////
 const pathIsDirectory = (ruta) => {
   return fs.lstatSync(ruta).isDirectory()
 }
-console.log(pathIsDirectory(process.argv[2]));
-
 ////////PREGUNTO SI ES UN ARCHIVO//////////
 const pathIsFile = function (ruta) {
   return fs.statSync(ruta).isFile()
 }
-console.log(pathIsFile(process.argv[2])); 
+//////////LEER DIRECTORIO O ARCHIVO DE FORMA RECURSIVA///////////
+const readDirectoryAndFile = (ruta) => {
+  let arrayResult = [];
+  if (pathIsDirectory(ruta)) {
+    arrayDirectory = fs.readdirSync(ruta);
+    arrayDirectory.forEach((archivo) => {
+      const routeList = path.join(ruta, archivo);
+      //console.log(routeList); 
+      if (pathIsDirectory(routeList)) {
+        arrayResult = arrayResult.concat(readDirectoryAndFile(routeList))
+      }
+      if (path.extname(routeList) === ".md") {
+        arrayResult.push(routeList)
+      }
+    })
+  } else {
+    arrayResult.push(ruta)
+  }
+  return arrayResult
+}
